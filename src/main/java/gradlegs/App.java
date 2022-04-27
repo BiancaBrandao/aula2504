07fe2d75c194261a17e4b5eb9443fd7b5108dd4a
BiancaBrandao <biancabrandao199@gmail.com>
2022-04-27 17:56:44 +0000

    seters e geters

---
+++ src/main/java/gradlegs/App.java
@@ -0,0 +1,12 @@
+package gradlegs;
+
+public class App {
+    public static void main(String args[]) {
+        Genero g = new Genero();
+        g.setId(-10);
+        g.setNome(nome: "Aventura");
+
+        System.out.println("Olá Gradle");
+        System.out.println(getNome.nome());
+    }
+}
